57861b586ed97651fa24d7e63960b13ea769dd0d
shaswata56 <shaswata56@gmail.com>
2021-09-26 21:32:38 +0600

    Completed Frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import useToken from "./useToken";
+import Auth from "./components/login/Auth";
+import useId from "./useId";
+import Dashboard from "./components/pages/Dashboard";
+import ReactLoading from 'react-loading'
+import './App.css'
+
+async function verify(token) {
+    let result = await fetch('http://localhost:8080/verify', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token
+        },
+    }).then(data => data.json())
+
+    return !result.error;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const { token, setToken } = useToken();
+    const { userid, setUserid } = useId();
+    const [ok,setOk] = useState(null);
+    const [done, setDone] = useState(undefined)
+
+    if(token === null) {
+        return <Auth setToken={setToken} setUserid={setUserid}/>;
+    }
+    verify(token)
+        .then(res => {
+            setDone(true);
+            setOk(res);
+        });
+
+    return(
+        <div className={"wrapper"}>
+            {!done ? (
+                <ReactLoading
+                    className={"loader"}
+                    type={"spin"}
+                    color={"#000000"}
+                />
+            ) :   ok ? (<Dashboard token={token} userid={userid}/>) : (<Auth setToken={setToken} setUserid={setUserid}/>)
+            }
+
+        </div>
+    );
 }
 
 export default App;
